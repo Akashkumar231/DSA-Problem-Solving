269eae34ac4b819e02659bb91329c213ad17a681
akashkumaryadav <ay355231@gmail.com>
2025-04-16 23:01:32 +0530

    updated dequ data structure

--- dequeue/src/Main.java
+++ dequeue/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Implementation of the DequeueOperation");
+        System.out.println("Implementation of the DequeueOperation.");
     }
 }
