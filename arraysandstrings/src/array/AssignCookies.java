ebdac18d3774d4a6530a973eeeeef3b3b7246c7d
akashkumaryadav <ay355231@gmail.com>
2025-05-11 00:33:23 +0530

    Solved Assign Cookie Problem Leetecode 455

---
+++ arraysandstrings/src/array/AssignCookies.java
@@ -0,0 +1,77 @@
+package array;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+
+    public static int findContentChildrenOptimise(int [] g , int [] s){
+
+        int cookie = 0;
+        int child = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while (child<g.length && cookie < s.length){
+
+            if (s[cookie]>= g[child]){
+                child++;
+            }
+            cookie++;
+
+        }
+
+        return child;
+    }
+
+    public  static int   findContentChildren(int[] g, int[] s) {
+//
+//        int []  g = {1,2};
+//        int [] s = {1,2,3};
+
+        boolean [] isSatified = new boolean[g.length];
+        boolean [] isAssigned = new boolean[s.length];
+
+         int count = 0;
+
+         for (int i = 0 ; i< g.length ; i++){
+
+             for (int j = 0 ; j< s.length ; j++){
+
+                 if (s[j] >= g[i] && !isAssigned[j]){
+                     count++;
+                     isAssigned[j]=true;
+                     break;
+                 }
+
+             }
+
+         }
+
+//        for (int i = 0;i< s.length ;i++){
+//
+//            for (int j = 0 ; j<g.length ; j++){
+//
+//                if (s[i]>=g[j] && isSatified[j]==false){
+//                    isSatified[j]=true;
+//                    count++;
+//                }
+//
+//
+//            }
+//
+//        }
+
+
+
+       return count;
+
+    }
+
+    public static void main(String [] args){
+      int []  g = {1,2};
+      int [] s = {1,2,3};
+      System.out.println(findContentChildrenOptimise(g,s));;
+    }
+
+}
