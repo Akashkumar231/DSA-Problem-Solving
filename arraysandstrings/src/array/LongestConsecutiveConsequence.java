5d9bddeddf583da13f4022af9fab92571424517a
akashkumaryadav <ay355231@gmail.com>
2025-05-25 19:00:04 +0530

    Added another method in LongestConsecutiveConsequence

--- arraysandstrings/src/array/LongestConsecutiveConsequence.java
+++ arraysandstrings/src/array/LongestConsecutiveConsequence.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class LongestConsecutiveConsequence {
 
     // This part gives time limit exceed error.
-    public static int getLongestConsecutiveConsequence(int [] array){
+    public static int getLongestConsecutiveConsequence1(int [] array){
         Set<Integer> hashset = new HashSet<>();
         int length = 0;
         int maxLength = Integer.MIN_VALUE;
@@ -31,16 +31,33 @@ public class LongestConsecutiveConsequence {
         return maxLength;
     }
 
-//    public static int getLongestConsecutiveConsequence2(int [] array){
-//
-//        Arrays.sort(array);
-//
-//
-//    }
+
+
+    public static int getLongestConsecutiveConsequence2(int [] array){
+        int length = 1;
+        Arrays.sort(array);
+        int element = array[0];
+        int longestSubsequence = Integer.MIN_VALUE;
+        for(int i = 0;i< array.length ;i++){
+
+            if (array[i]==element+1){
+                length++;
+                element+=1;
+                longestSubsequence = Math.max(length,longestSubsequence);
+            }
+            else if (element!=array[i]){
+                 element=array[i];
+                 length=1;
+            }
+
+        }
+       return longestSubsequence;
+    }
     public static void main(String []args){
 
         int [] array = new int[]{102,4,100,1,101,3,2,1,1};
-        System.out.println(getLongestConsecutiveConsequence(array));
+        System.out.println(getLongestConsecutiveConsequence1(array));
+        System.out.println(getLongestConsecutiveConsequence2(array));
 
     }
 
