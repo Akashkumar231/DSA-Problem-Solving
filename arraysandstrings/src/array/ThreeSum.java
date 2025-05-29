81f636d431d7b3b049a33e48419f260c26208630
akashkumaryadav <ay355231@gmail.com>
2025-05-30 01:09:07 +0530

    Solved Problem

--- arraysandstrings/src/array/ThreeSum.java
+++ arraysandstrings/src/array/ThreeSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class ThreeSum {
 
-    public static List<List<Integer>> threeSum1(int[] nums) {
+    public static List<List<Integer>> threeSum1(int[] nums){
 
         List<List<Integer>> ansList = new ArrayList<>();
         Set<List<Integer>> set = new HashSet<>();
@@ -30,8 +30,7 @@ public class ThreeSum {
         return ansList;
     }
 
-    public static List<List<Integer>> threeSum2(int [] nums)
-    {
+    public static List<List<Integer>> threeSum2(int [] nums){
         Set<List<Integer>> hashSet =new HashSet<>();
         for (int i = 0 ; i< nums.length ; i++){
             Set<Integer> elements = new HashSet<>();
@@ -56,11 +55,42 @@ public class ThreeSum {
         return list;
     }
 
+    public static List<List<Integer>> threeSum3(int [] nums){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0 ; i< nums.length ; i++){
+            if (i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int j = i + 1 ;
+            int k = nums.length -1;
+            while (j<k){
+                int sum  = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while (j<k && nums[j] == nums[j-1]) j++;
+                    while (j<k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String [] args){
 
         int [] nums = new int []{-1,0,1,2,-1,-4};
 
-        List<List<Integer>> ans= threeSum2(nums);
+        List<List<Integer>> ans= threeSum3(nums);
 
         for (List<Integer> integers : ans){
             System.out.println(integers);
