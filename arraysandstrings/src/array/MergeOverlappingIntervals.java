e092825af597c01a60fbcebc690ef1b6110f9085
akashkumaryadav <ay355231@gmail.com>
2025-06-02 00:18:09 +0530

    Provided the Optimized Solution

--- arraysandstrings/src/array/MergeOverlappingIntervals.java
+++ arraysandstrings/src/array/MergeOverlappingIntervals.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class MergeOverlappingIntervals {
 
-    public static List<List<Integer>> getOverlappingIntervals(int [][] array ){
+    public static List<List<Integer>> getOverlappingIntervals1(int [][] array ){
 
         Arrays.sort(array, Comparator.comparingDouble(o -> o[0]));
         List<List<Integer>> arrayList = new ArrayList<>();
@@ -32,12 +32,27 @@ public class MergeOverlappingIntervals {
         return arrayList;
 
     }
+    public static List<List<Integer>> getOverlappingIntervals2(int [][] array )
+    {
+        Arrays.sort(array, Comparator.comparingDouble(o -> o[0]));
+        List<List<Integer>> arrayList = new ArrayList<>();
+        for(int i = 0 ; i< array.length ; i++){
+            if (arrayList.isEmpty() || array[i][0]>arrayList.getLast().get(1)){
+                arrayList.add( Arrays.asList(array[i][0],array[i][1]));
+            }
+            else{
+                int element=Math.max(array[i][1],arrayList.getLast().get(1));
+                arrayList.getLast().set(1,element);
+            }
+        }
+        return arrayList;
+    }
 
     public static void main(String [] args){
 
         int [][] array = new int[][]{{1,3},{2,6},{8,10},{15,18}};
         Arrays.sort(array, Comparator.comparingDouble(o -> o[0]));
-        List<List<Integer>> ansList = getOverlappingIntervals(array);
+        List<List<Integer>> ansList = getOverlappingIntervals2(array);
         for (List<Integer> elements :ansList){
             System.out.print(elements+ " ");
         }
