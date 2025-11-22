292de69abbd1760b18c5a9a8a59a25715add3374
akashkumar_yadav <ay355231@gmail.com>
2025-11-23 08:28:07 +1400

    Solved Voting_Algorithm.

--- arraysandstrings/src/array/Voting_Algorithm.java
+++ arraysandstrings/src/array/Voting_Algorithm.java
@@ -2,7 +2,33 @@ package array;
 
 public class Voting_Algorithm {
 
+    public static int getMajority(int [] array){
+
+        int majority = array[0];
+        int count = 1;
+
+        for (int i = 1 ; i<array.length ; i++){
+
+            if (majority!=array[i]){
+                count--;
+            }else if (majority==array[i]){
+                count++;
+            }
+            if (count==0){
+                majority=array[i];
+                count=1;
+            }
+
+        }
+
+        return majority;
+
+    }
+
     public static void main(String [] args){
+
+        int [] array = new int[]{10,10,20};
+        System.out.println(getMajority(array));
 
     }
 
