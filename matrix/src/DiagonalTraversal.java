d41825cecbef50f03714a2e83693b9163ae724bf
Akashkumar Yadav <ay355231@gmail.com>
2025-12-14 10:47:29 +0430

    Implemented Diagonal Traversal

--- matrix/src/DiagonalTraversal.java
+++ matrix/src/DiagonalTraversal.java
@@ -1,7 +1,24 @@
 public class DiagonalTraversal {
 
     public static void main(String [] args){
+        Matrix.printMatrix();
+        int m = Matrix.matrix.length ;
+        int n = Matrix.matrix[0].length;
 
+
+        // iterating over the diagonal.
+        for (int diagonal= 0 ;  diagonal<=m+n-2; diagonal++){
+
+            int row = (diagonal<n) ? 0 : diagonal -n +1;
+            int col = (diagonal<n) ? diagonal : n-1;
+            while(row<m && col>=0){
+                System.out.print(Matrix.matrix[row][col] + " ");
+                row++;
+                col--;
+            }
+            System.out.println();
+
+        }
     }
 
 }
