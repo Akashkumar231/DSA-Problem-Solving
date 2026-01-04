293922c8df5cd82e9630761b30963abbdc5f6cbf
Akashkumar Yadav <ay355231@gmail.com>
2026-01-04 15:06:07 +0430

    Solved binary search question

---
+++ binarysearch/src/PeakIndexInMountainArray.java
@@ -0,0 +1,24 @@
+public class PeakIndexInMountainArray {
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int left = 1;
+        int right = arr.length - 2;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if( arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid]){
+                return mid;
+            }else if(arr[mid] > arr[mid-1]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String [] args){
+      int [] array = new int[]{0,10,5,2};
+        System.out.println(peakIndexInMountainArray(array));
+    }
+
+}
---
+++ binarysearch/src/findPeakElement.java
@@ -0,0 +1,32 @@
+public class findPeakElement {
+
+    public static int peakElement(int [] array){
+
+        int left = 0;
+        int right = array.length - 1;
+        if (array[left] < array[left+1]){
+            return left;
+        }
+        if (array[right] > array[right-1]){
+            return right;
+        }
+
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if( array[mid-1] < array[mid] && array[mid+1] < array[mid]){
+                return mid;
+            }else if(array[mid] > array[mid-1]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String [] args){
+
+    }
+}
