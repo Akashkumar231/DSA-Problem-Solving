badc3577ff0c318e72880470069d1c3b0deba852
Akashkumar Yadav <ay355231@gmail.com>
2026-02-05 23:46:53 +0530

    Solved Leetecode Question

---
+++ leetecodeDailyProblem/src/Transformed_Array.java
@@ -0,0 +1,11 @@
+public class Transformed_Array {
+    public int[] constructTransformedArray(int[] A) {
+        int n = A.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++)
+            res[i] = A[(((i + A[i]) % n) + n) % n];
+
+        return res;
+    }
+}
