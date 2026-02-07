8e0ae006dc7ef37e385475229362e5bc0aa728fa
Akashkumar Yadav <ay355231@gmail.com>
2026-02-08 00:04:04 +0530

    Solved Leetecode Question.

---
+++ leetecodeDailyProblem/src/Minimum_Deletions.java
@@ -0,0 +1,18 @@
+public class Minimum_Deletions {
+
+    public int minimumDeletions(String s) {
+        int res = 0, b = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'b')
+                b++;
+            else if (b > 0) {
+                res++;
+                b--;
+            }
+        }
+
+        return res;
+    }
+}
