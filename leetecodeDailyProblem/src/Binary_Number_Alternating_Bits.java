941cd3a77cc9289daa45d7e5aae3f702383a9ff3
Akashkumar Yadav <ay355231@gmail.com>
2026-02-20 23:13:59 +0530

    Solved Leetecode Question

--- leetecodeDailyProblem/src/Binary_Number_Alternating_Bits.java
+++ leetecodeDailyProblem/src/Binary_Number_Alternating_Bits.java
@@ -21,6 +21,12 @@ public class Binary_Number_Alternating_Bits {
         return true;
     }
 
+        public boolean hasAlternatingBits(int n) {
+            int x = n ^ (n >> 1);
+            return (x & (x + 1)) == 0;
+        }
+
+
     public static void main(String [] args){
         System.out.println(isAlternate(5
         ));
