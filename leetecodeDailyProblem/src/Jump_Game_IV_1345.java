4ea0fab016c670dbc2bcf64f0bcada17807e51eb
Akashkumar Yadav <ay355231@gmail.com>
2026-05-20 00:04:28 +0530

    Solved Leetecode Question

---
+++ leetecodeDailyProblem/src/Jump_Game_IV_1345.java
@@ -0,0 +1,26 @@
+public class Jump_Game_IV_1345 {
+
+
+
+    public  static int minJumps(int[] arr) {
+        if(arr.length == 1){
+            return 0;
+        }
+
+        if(arr[0]== arr[arr.length-1]){
+            return 1;
+        }
+
+        for(int index = 0 ; index < arr.length ; index++ ){
+
+
+        }
+
+
+    }
+
+    public static void main(String [] args){
+
+    }
+
+}
